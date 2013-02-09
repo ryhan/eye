2c1337c485ffce945a68faae0953d314bbe05a00
Ryhan <rthprog@Gmail.com>
2013-02-09 18:10:16 -0500

    working demo

--- eye.js
+++ eye.js
@@ -22,6 +22,14 @@ $(window).load(function() {
     alert(error);
   }
 
+  is_on_left = 0;
+  is_on_right = 0;
+  cant_see = 0;
+  left_threshold = 250;
+  right_threshold = 250;
+
+  wait_threshold = 5;
+
   function tick() {
     compatibility.requestAnimationFrame(tick);
     
@@ -29,9 +37,33 @@ $(window).load(function() {
       $(video).objectdetect("all", {scaleMin: 3, scaleFactor: 1.1, classifier: objectdetect.frontalface}, function(coords) {
         if (coords[0]) {
           coords = smoother.smooth(coords[0]);
-          console.log(coords);
-        } else {
-          console.log("none");
+
+          if (coords[2] > left_threshold){
+            is_on_left++;
+          }
+          if (coords[2] < right_threshold){
+            is_on_right++;
+          }
+          if (is_on_left == wait_threshold){
+            $('.eye').animate({left: '-300px'}, 1000);
+            is_on_right = 0;
+          }
+          if (is_on_right == wait_threshold){
+            $('.eye').animate({left: '300px'}, 1000);
+            is_on_left = 0;
+          }
+          $('.cansee').hide();
+          
+          console.log("score :" + coords[2] + ", right: " + is_on_right + ", left: " + is_on_left);
+        }else{
+
+          cant_see++;
+          if (cant_see == wait_threshold){
+            $('.eye').animate({left: '0px'}, 1000);
+            $('.cansee').show();
+            cant_see = 0;
+          }
+          console.log("can't see you!");
         }
       });
     }
